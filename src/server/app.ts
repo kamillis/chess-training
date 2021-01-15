21e21ee93a9e2046d64faec035aa048e155bf09d
kamil-lis <kamil.lis@schibsted.pl>
2021-01-15 20:46:53 +0100

    Initial commit

---
+++ src/server/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import compression from "compression";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(compression());
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Chess training app listening at http://localhost:${port}`);
+});

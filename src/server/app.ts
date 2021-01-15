dec69cb62dae9dab400053a8cb0631315681585a
kamil-lis <kamil.lis@schibsted.pl>
2021-01-15 21:05:58 +0100

    First deployable version

--- src/server/app.ts
+++ src/server/app.ts
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(compression());
-app.use(express.static("public"));
+app.use(express.static("dist"));
 
 app.listen(port, () => {
   console.log(`Chess training app listening at http://localhost:${port}`);

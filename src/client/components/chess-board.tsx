c6b8f682f9177ebc7085a5aa91b7223977d68b75
kamil-lis <kamil.lis@schibsted.pl>
2021-01-16 12:35:23 +0100

    Add counters

--- src/client/components/chess-board.tsx
+++ src/client/components/chess-board.tsx
@@ -7,6 +7,9 @@ type PropsType = {
   onSetPosition: (boardElement: BoardElement, from: null | BoardPosition, to: BoardPosition) => void
 };
 
+const sideNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const bottomLetters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
 // const getBoardPosition = (color: "white" | "black", rowIndex: number, colIndex: number): BoardPosition => {
 //   return [4, 4];
 // };
@@ -15,20 +18,28 @@ export const ChessBoard: React.FC<PropsType> = ({ data, onSetPosition }) => {
   const [picked, setPicked] = React.useState<{ boardElement: BoardElement, from: null | BoardPosition } | null>(null);
 
   return (
-    <div className={`chess-board ${data.color}-bottom`}>
-      {data.board.map((row, rowIndex) => (
-        <div key={rowIndex} className={`board-row row-${rowIndex % 2}`}>
-          {row.map((col, colIndex) => {
-            const onClick = () => {};
+    <div className="chess-board-container">
+      <div className={`side-counter ${data.color}-bottom`}>
+        {sideNumbers.map(number => <div key={number}>{number}</div>)}
+      </div>
+      <div className={`bottom-counter ${data.color}-bottom`}>
+        {bottomLetters.map(letter => <div key={letter}>{letter}</div>)}
+      </div>
+      <div className={`chess-board ${data.color}-bottom`}>
+        {data.board.map((row, rowIndex) => (
+          <div key={rowIndex} className={`board-row row-${rowIndex % 2}`}>
+            {row.map((col, colIndex) => {
+              const onClick = () => {};
 
-            return (
-              <div key={colIndex} className={`board-field col-${colIndex % 2}`} onClick={onClick}>
-                {col}
-              </div>
-            );
-          })}
-        </div>
-      ))}
+              return (
+                <div key={colIndex} className={`board-field col-${colIndex % 2}`} onClick={onClick}>
+                  {col}
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

7b1e1319f49202236db18abaeaaa36e97225473a
kamil-lis <kamil.lis@schibsted.pl>
2021-01-16 13:53:24 +0100

    Add moving

--- src/client/components/chess-board.tsx
+++ src/client/components/chess-board.tsx
@@ -12,10 +12,6 @@ type PropsType = {
 const sideNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
 const bottomLetters = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
-// const getBoardPosition = (color: "white" | "black", rowIndex: number, colIndex: number): BoardPosition => {
-//   return [4, 4];
-// };
-
 export const ChessBoard: React.FC<PropsType> = ({ data, onSetPosition }) => {
   const [picked, setPicked] = React.useState<{ boardElement: BoardElement, from: null | BoardPosition } | null>(null);
 
@@ -32,7 +28,14 @@ export const ChessBoard: React.FC<PropsType> = ({ data, onSetPosition }) => {
         {data.board.map((row, rowIndex) => (
           <div key={rowIndex} className={`board-row row-${rowIndex % 2}`}>
             {row.map((col, colIndex) => {
-              const onClick = () => { console.log(rowIndex, colIndex) };
+              const onClick = () => {
+                if (picked) {
+                  setPicked(null);
+                  onSetPosition(picked.boardElement, picked.from, [rowIndex, colIndex]);
+                } else if (col !== "") {
+                  setPicked({ boardElement: col, from: [rowIndex, colIndex] });
+                }
+              };
 
               return (
                 <div key={colIndex} className={`board-field col-${colIndex % 2}`} onClick={onClick}>
